0896752fea6b3a63f37b83dd375bff616fc31f66
Deepanshu Pariyani <pariyanideepanshu@gmail.com>
2024-03-31 16:16:56 +0530

    routes added

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,6 +3,13 @@ import { loginUser, logout, refreshAccessToken, registerUser } from "../controll
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import {upload} from "../middlewares/multer.js"
 import {ApiError} from "../utils/ApiError.js"
+import { changeCurrentPassword } from "../controllers/user.controller.js";
+import { getCurrentUser } from "../controllers/user.controller.js";
+import { updateAccountDetails } from "../controllers/user.controller.js";
+import { updateUserAvatar } from "../controllers/user.controller.js";
+import { updateUserCoverImage } from "../controllers/user.controller.js";
+import { getUserChannelProfile } from "../controllers/user.controller.js";
+import { getWatchHistory } from "../controllers/user.controller.js";
 const router=Router();
 
 router.route("/register").post(
@@ -21,5 +28,14 @@ router.route("/register").post(
     router.route("/login").post(loginUser);
     router.route("/logout").post(verifyJWT,logout);
     router.route("/refresh-token").post(refreshAccessToken);
+    router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+    router.route("/current-user").get(verifyJWT, getCurrentUser)
+    router.route("/update-account").patch(verifyJWT, updateAccountDetails)
+
+    router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+    router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
+
+    router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+    router.route("/history").get(verifyJWT, getWatchHistory)
 
 export default router;
